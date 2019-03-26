ffdf9df60e4a52d0925b07e1745450ff6cf8e6c5
Mohammad Ali <mohalikhan@hotmail.com>
2019-03-26 15:54:30 -0400

    Add more function for sorting and warmup.

--- Solution.cs
+++ Solution.cs
@@ -1,193 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading;
 
-namespace ConsoleApp1
+namespace CodePractice
 {
     class Solution
     {
-        // Complete the sockMerchant function below.
-        private static int sockMerchant(int n, int[] ar)
-        {
-            var toReturn = 0;
-            var list = new List<int>();
-            for (var i = 0; i < n; i++)
-            {
-                if (list.Contains(ar[i]))
-                {
-                    toReturn++;
-                    list.RemoveAt(list.IndexOf(ar[i]));
-                }
-                else
-                {
-                    list.Add(ar[i]);
-                }
-            }
-
-            return toReturn;
-        }
-
-        // add array together
-        private static int simpleArraySum(int[] ar)
-        {
-            var toReturn = 0;
-            for (var i = 0; i < ar.Length; i++)
-            {
-                toReturn += ar[i];
-            }
-
-            return toReturn;
-        }
-
-        // Complete the compareTriplets function below.
-        private static List<int> compareTriplets(List<int> a, List<int> b)
-        {
-            var bob = 0;
-            var alice = 0;
-            for (var i = 0; i < a.Count; i++)
-            {
-                if (a[i] > b[i])
-                {
-                    alice++;
-                }
-
-                if (a[i] < b[i])
-                {
-                    bob++;
-                }
-            }
-            return new List<int> { alice, bob };
-        }
-
-        // Complete the aVeryBigSum function below.
-        private static long aVeryBigSum(long[] ar)
-        {
-            long toReturn = 0;
-            for (var i = 0; i < ar.Length; i++)
-            {
-                toReturn += ar[i];
-            }
-
-            return toReturn;
-
-        }
-
-        // Complete the diagonalDifference function below.
-        private static int diagonalDifference(int[][] arr)
-        {
-            var first = 0;
-            var second = 0;
-            for (var i = 0; i < arr.Length; i++)
-            {
-                for (var j = 0; j < arr.Length; j++)
-                {
-                    if (i == j)
-                        first += arr[i][j];
-                    if ((i + j) == (arr.Length - 1))
-                        second += arr[i][j];
-                }
-            }
-
-            return Math.Abs(first - second);
-        }
-
-        //Plus Minus (algorithm) problem
-        private static void plusMinusProblem(int[] arr)
-        {
-            var positiveNumbers = 0;
-            var negativeNumbers = 0;
-            var zeroNumbers = 0;
-
-            for (var i = 0; i < arr.Length; i++)
-            {
-                if (arr[i] > 0)
-                    ++positiveNumbers;
-                else if (arr[i] < 0)
-                    ++negativeNumbers;
-                else
-                    ++zeroNumbers;
-            }
-
-            Console.WriteLine((double)positiveNumbers / arr.Length);
-            Console.WriteLine((double)negativeNumbers / arr.Length);
-            Console.WriteLine((double)zeroNumbers / arr.Length);
-        }
-
-        // Staircase problem
-        private static void staircaseProblem(int n)
-        {
-            for (var i = 1; i <= n; i++)
-            {
-                var spaces = new string(' ', n - i);
-                var hashes = new string('#', i);
-                Console.WriteLine(spaces + hashes);
-            }
-        }
-
-        //Mini-Max Sum problem
-        private static void mini_maxProblem(int[] numbers)
-        {
-            var sumOfAllNumbers = 0L;
-            var minimum = long.MaxValue;
-            var maximum = 0L;
-            for (var i = 0; i < 5; i++)
-            {
-                sumOfAllNumbers += numbers[i];
-                if (numbers[i] < minimum)
-                    minimum = numbers[i];
-
-                if (numbers[i] > maximum)
-                    maximum = numbers[i];
-
-            }
-            Console.WriteLine($"{sumOfAllNumbers - maximum} {sumOfAllNumbers - minimum}");
-        }
-
-        private static string timeConversion(string time)
-        {
-            var amOrPm = time.Substring(8);
-            var hourComponent = time.Substring(0, 2);
-            var remainingTimeComponent = time.Substring(2, 6);
-            if (amOrPm == "AM" && hourComponent == "12")
-            {
-                hourComponent = "00";
-            }
-            else if (amOrPm == "PM")
-            {
-                var numericHourComponent = int.Parse(hourComponent);
-                if (numericHourComponent != 12)
-                {
-                    hourComponent = Convert.ToString(12 + numericHourComponent);
-                }
-            }
-
-            return hourComponent + remainingTimeComponent;
-        }
-
-        // Birthday cake candles problem
-        private static int birthdayCakeCandles(int[] ar)
-        {
-            var maxValue = ar[0];
-            var maxValueOccurence = 1;
-
-            for (int i = 1; i < ar.Length; i++)
-            {
-                if (ar[i] == maxValue)
-                {
-                    maxValueOccurence++;
-                    continue;
-                }
-                if (ar[i] > maxValue)
-                {
-                    maxValue = ar[i];
-                    maxValueOccurence = 1;
-                }
-            }
-            return maxValueOccurence;
-        }
-
         static void Main(string[] args)
         {
 
@@ -211,7 +27,10 @@ namespace ConsoleApp1
                 arr[i] = Array.ConvertAll(Console.ReadLine().Split(' '), Convert.ToInt32);
             }*/
 
-            plusMinusProblem(ar);
+            //sorting array
+
+
+            Warmup.plusMinusProblem(ar);
 
             //Console.WriteLine(result);
             //Console.WriteLine(string.Join(" ", result));
---
+++ Sorting.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    internal class Sorting
+    {
+        //bigSorting problem
+        public static string[] bigSorting(string[] unsorted)
+        {
+            Array.Sort(unsorted, new CustomComparer());
+            return unsorted;
+        }
+
+        // Complete the countingSort  1 function below.
+        public static int[] countingSort(int[] arr)
+        {
+            var output = new int[100];
+            foreach (var i in arr)
+                output[i] += 1;
+
+            return output;
+        }
+
+        // Complete the countingSort 2 function below.
+        public static int[] countingSort2(int[] arr)
+        {
+            var numberFrequency = new int[100];
+            foreach (var i in arr)
+                numberFrequency[i] += 1;
+
+            for (var i = 1; i < 100; i++)
+                numberFrequency[i] += numberFrequency[i - 1];
+
+            var sortedOutput = new int[arr.Length];
+
+            foreach (var num in arr)
+            {
+                var position = numberFrequency[num];
+                numberFrequency[num] -= 1;
+                sortedOutput[position - 1] = num;
+            }
+            return sortedOutput;
+        }
+
+        // Complete the quickSort function below.
+        public static int[] QuickSort(int[] arr)
+        {
+            var pivot = arr[0];
+            var lessList = new List<int>();
+            var equalList = new List<int>();
+            var moreList = new List<int>();
+            var outputArr = new int[arr.Length];
+
+            equalList.Add(arr[0]);
+
+            for (var i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] < pivot)
+                    lessList.Add(arr[i]);
+                else if (arr[i] > pivot)
+                {
+                    moreList.Add(arr[i]);
+                }
+                else
+                {
+                    equalList.Add(arr[i]);
+                }
+            }
+
+            var j = 0;
+
+            foreach (var item in lessList)
+                outputArr[j++] = item;
+
+            foreach (var item in equalList)
+                outputArr[j++] = item;
+
+            foreach (var item in moreList)
+                outputArr[j++] = item;
+
+
+            return outputArr;
+        }
+
+        static int[] QuickSort2(int[] arr)
+        {
+            var pivot = arr[0];
+            var smallerItems = new List<int>();
+            var equalItems = new List<int>();
+            var biggerItems = new List<int>();
+            var outputArr = new int[arr.Length];
+
+            equalItems.Add(arr[0]);
+
+            for (var i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] < pivot)
+                    smallerItems.Add(arr[i]);
+                else if (arr[i] > pivot)
+                {
+                    biggerItems.Add(arr[i]);
+                }
+                else
+                {
+                    equalItems.Add(arr[i]);
+                }
+            }
+
+            if (smallerItems.Count > 1)
+                smallerItems = QuickSort2(smallerItems.ToArray()).ToList();
+
+            if (biggerItems.Count > 1)
+                biggerItems = QuickSort2(biggerItems.ToArray()).ToList();
+
+            var j = 0;
+
+            foreach (var item in smallerItems)
+            {
+                outputArr[j++] = item;
+                Console.Write(item);
+                Console.Write(' ');
+            }
+
+            foreach (var item in equalItems)
+            {
+                outputArr[j++] = item;
+                Console.Write(item);
+                Console.Write(' ');
+            }
+
+            foreach (var item in biggerItems)
+            {
+                outputArr[j++] = item;
+                Console.Write(item);
+                Console.Write(' ');
+            }
+
+            Console.WriteLine();
+            return outputArr;
+        }
+
+        // Complete the runningTime function below.
+        static int runningTime(int[] arr)
+        {
+            var shiftCount = 0;
+            for (var i = 1; i < arr.Length; i++)
+            {
+                var value = arr[i];
+                var j = i - 1;
+                while (j >= 0 && value < arr[j])
+                {
+                    arr[j + 1] = arr[j];
+                    shiftCount++;
+                    j = j - 1;
+                }
+                arr[j + 1] = value;
+            }
+            return shiftCount;
+
+        }
+    }
+
+    public class CustomComparer : IComparer<string>
+    {
+        public int Compare(string x, string y)
+        {
+            if (x.Length < y.Length)
+                return -1;
+            if (x.Length > y.Length)
+            {
+                return 1;
+            }
+
+            if (x == y)
+                return 0;
+            var i = 0;
+            while (x[i] == y[i]) i++;
+            return x[i].CompareTo(y[i]);
+        }
+    }
+}
